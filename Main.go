20f796a951208c47e2589a8e69931014e17f5ad9
anil1596 <anil.khadwal@gmail.com>
2016-06-07 22:39:14 +0530

    Pre-Final

--- Main.go
+++ Main.go
@@ -55,22 +55,34 @@ func (f File) getStrings() (string, string, string) {
 
 func main() {
 	var err error
+	mainchan := make(chan bool)
+
 	workingdir, err = filepath.Abs(filepath.Dir(os.Args[0])) //getting file path from run time
-	fmt.Println(workingdir)
+	jsonpath = string(os.Args[1])
+
 	if err != nil {
 		log.Fatal(err)
 	}
+	runner()
+	<-mainchan
+}
 
-	files, selfobserve := getFiles(workingdir + "/files.json")
-
+func runner() {
+	// fmt.Println("runner called")
+	files, selfobserve := getFiles(jsonpath)
+	fmt.Println("after get files ,self oberve is ,", selfobserve)
+	fmt.Println(globallock)
 	globallock.Lock()
-	reqmap = observercreator(files)
+	// fmt.Println("acuired globallock")
+	recordmap = observercreator(files)
 	globallock.Unlock()
+	// fmt.Println("globalock released ")
 
 	startOberver(selfobserve)
 }
 
-var reqmap map[string]observedfile
+var jsonpath string
+var recordmap map[string]observedfile
 var workingdir string
 var globallock = &sync.Mutex{}
 
@@ -84,9 +96,9 @@ func observercreator(files []File) map[string]observedfile {
 			m = workingdir + m
 			n = workingdir + n
 		}
-
+		channel := make(chan bool)
 		eventlist := make(map[string]int64) //to initialise Blank eventlist
-		newfile := observedfile{eventlist: eventlist, onchange: n}
+		newfile := observedfile{eventlist: eventlist, onchange: n, done: channel}
 		temp[m] = newfile
 
 	}
@@ -97,87 +109,59 @@ func startOberver(selfobserve bool) {
 	if selfobserve {
 		//**************to monitor .json file
 		eventlist := make(map[string]int64)
-		jsonobj := observedfile{eventlist: eventlist, onchange: workingdir + "/actions/json.sh"}
-		for key, value := range reqmap {
-			go value.watch(key)
-		}
+		channel := make(chan bool)
+		jsonobj := observedfile{eventlist: eventlist, onchange: workingdir + "/actions/json.sh", done: channel}
 
 		globallock.Lock()
-		reqmap[workingdir+"/files.json"] = jsonobj
+		// fmt.Println("\nacquired globallock before  go routine in startOberver", globallock)
+		for key, _ := range recordmap {
+			go watch(key)
+		}
 		globallock.Unlock()
+		// fmt.Println("\nreleased globallock after  go routine in startOberver", globallock)
 
-		jsonobj.watch(workingdir + "/files.json")
+		globallock.Lock()
+		// fmt.Println("\nacuired globallock before  json watch in startOberver", globallock)
+		recordmap[jsonpath] = jsonobj
+		globallock.Unlock()
+		// fmt.Println("\nreleased globallock after  json watch in startOberver", globallock)
+		watch(jsonpath)
 
 	} else { //if not to monitor json file
 		i := 0
-		for key, value := range reqmap {
-			if i == len(reqmap) {
-				value.watch(key)
+		globallock.Lock()
+		// fmt.Println("\nacquired globallock before  go routine in startOberver (selfobserve false )", globallock)
+		for key, _ := range recordmap {
+
+			if i == len(recordmap) {
+				watch(key)
 			}
+
 			i++
-			go value.watch(key)
+			go watch(key)
 		}
+		globallock.Unlock()
+		// fmt.Println("\nreleased globallock after  go routine in startOberver(selfobserve false)", globallock)
 	}
 }
 
 //***********structure used for creating attributes to monitor file********
 type observedfile struct {
-	eventlist map[string]int64 // map to store events("MODIFY","DELETE") and their timestamps
-	onchange  string           //map to store workingdir PATH and exec EVENT file path map("workingdir":"exec")
-	mutex     sync.Mutex
-	mywatcher *fsnotify.Watcher
-}
-
-//************functions accessed by only instance of structure i.e. observedfile
-func execute(obj string) { // executes events of the events File
-	len := len(reqmap[obj].eventlist) //to check either eventlist is empty or not
-	if len != 0 {
-		fmt.Println("inevent executioin")
-		if obj == workingdir+"/files.json" {
-			destroytillnow()
-			main()
-		}
-		// fmt.Println("\n\neventlist of ", obj.address["workingdir"], " :", obj.eventlist, "at", time.Now().Unix())
-		out, err := exec.Command(reqmap[obj].onchange).Output()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("\n\nevent_list :", string(out), " : executed on: ", obj, "at", time.Now().Unix())
-		delete(reqmap[obj].eventlist, "MODIFY")
-		delete(reqmap[obj].eventlist, "DELETE")
-	}
-
-}
-func destroytillnow() {
-	for key, value := range reqmap {
-		fmt.Println("\n\nREMOVING WATCHER :", value.mywatcher)
-		value.mywatcher.RemoveWatch(key)
-	}
-	<-time.After(time.Second * 3) //time given to stop all concurrently running watchers
-
-	for key, _ := range reqmap { //delete the previous values of reqmap
-		globallock.Lock()
-		delete(reqmap, key)
-		globallock.Unlock()
-	}
-	fmt.Println(reqmap)
-
-}
-
-//*************** To synchronize eventlist
-func queue(obj string) {
-	<-time.After(time.Millisecond * 200) //wait when an event is fired
-	execute(obj)
+	eventlist map[string]int64  // map to store events("MODIFY","DELETE") and their timestamps
+	onchange  string            //map to store workingdir PATH and exec EVENT file path map("workingdir":"exec")
+	mutex     sync.Mutex        //mutex is lock provided to each instance of structure
+	mywatcher *fsnotify.Watcher //watcher for each instance of structure
+	done      chan bool
 }
 
 //**********function to implement watcher(fsnotify)************
-func (current observedfile) watch(obj string) {
+func watch(obj string) {
+	// done := make(chan bool)
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("\n\nwatching: ", obj)
-	done := make(chan bool)
 
 	err = watcher.Watch(obj)
 	if err != nil {
@@ -185,48 +169,47 @@ func (current observedfile) watch(obj string) {
 
 		log.Fatal(err)
 	}
-
+	current := recordmap[obj]
 	current.mywatcher = watcher
-	reqmap[obj] = current
-	// fmt.Println(current.mywatcher)
-	// fmt.Println(reqmap[obj].mywatcher)
 
+	globallock.Lock()
+	// fmt.Println("\nacquired globallock before  assigning current to", obj, " :", globallock)
+	recordmap[obj] = current
+	globallock.Unlock()
+	// fmt.Println("\nreleased globallock after  assigning current to", obj, " :", globallock)
 	go func() {
 		for {
 			select {
 			case ev := <-watcher.Event:
-
-				//				fmt.Println("\n****************    ", ev.Name, "        ********************")
 				if ev.IsModify() {
 					fmt.Println("\nevent:", ev, " at time:", time.Now())
 
-					current.mutex.Lock()
-					fmt.Println("before updating eventlist")
-					fmt.Println(current.eventlist)
+					globallock.Lock()
+					// fmt.Println("\nacquired globallock before updating eventlist of ", obj, " :", globallock)
+					fmt.Printf("\n%#v", current)
 					current.eventlist["MODIFY"] = time.Now().Unix() //update modification timestamp  in structure eventlist
-					fmt.Println("after updating eventlist")
-					fmt.Println(current.eventlist)
-					current.mutex.Unlock()
+					globallock.Unlock()
 
-					current.mutex.Lock()
-					queue(obj)
-					current.mutex.Unlock()
+					// fmt.Println("\nreleased globallock after updating eventlist of ", obj, " :", globallock)
+					process_queue(obj) //execute the eventlist
 
 				}
 				if ev.IsDelete() {
 					fmt.Println("\nevent:", ev, " at time:", time.Now())
-					current.mutex.Lock()
+
+					globallock.Lock()
+					// fmt.Println("\nacquired globallock before updating eventlist of ", obj, " :", globallock)
+					fmt.Printf("%#v", current)
 					current.eventlist["DELETE"] = time.Now().Unix() //update deletion timestamp  in structure eventlist
-					current.mutex.Unlock()
+					globallock.Unlock()
 
-					watcher.RemoveWatch(obj)
+					// fmt.Println("\nreleased globallock after updating eventlist of ", obj, " :", globallock)
+					watcher.RemoveWatch(obj) //remove watcher whenever DELETE event occurs
 
-					current.mutex.Lock()
-					queue(obj)
-					current.mutex.Unlock()
+					process_queue(obj) //execute the eventlist
 
-					go current.watch(obj)
-					done <- true
+					go watch(obj)
+					current.done <- true
 				}
 			case err := <-watcher.Error:
 				fmt.Println("\nfile checking has error")
@@ -234,6 +217,63 @@ func (current observedfile) watch(obj string) {
 			}
 		}
 	}()
-	<-done
+	<-current.done
+
 	fmt.Println("\nwatcher closed on :", obj)
+}
+
+//***********functions to work on key of recordmap so that it can access value(observedfile) pointed by that particular key
+
+//*************** To synchronize eventlist
+
+func process_queue(obj string) {
+	<-time.After(time.Millisecond * 200) //wait when an event is fired
+	execute(obj)
+}
+func execute(obj string) { // executes events of the events File
+	globallock.Lock()
+	fmt.Println("PROCESS QUEUE HAS BEEN CALLED")
+	len := len(recordmap[obj].eventlist) //to check either eventlist is empty or not
+	flag := false
+	if len != 0 {
+
+		if obj == jsonpath {
+			destroytillnow()
+			flag = true
+		}
+
+		if !flag {
+			_, err := exec.Command(recordmap[obj].onchange).Output()
+			if err != nil {
+				fmt.Println()
+				log.Fatal(err)
+			}
+
+			fmt.Printf("\n\nevent_list  executed on: ", obj, "at", time.Now().Unix())
+			delete(recordmap[obj].eventlist, "MODIFY")
+			delete(recordmap[obj].eventlist, "DELETE")
+		}
+	}
+	globallock.Unlock()
+	if flag {
+		runner()
+	}
+}
+
+func destroytillnow() {
+
+	for key, value := range recordmap {
+		fmt.Println("\n\nREMOVING WATCHER on :", key)
+
+		value.mywatcher.RemoveWatch(key)
+		value.done <- true
+
+	}
+	//<-time.After(time.Second * 4) //time given to stop all concurrently running watchers
+
+	for key, _ := range recordmap { //delete the previous values of recordmap
+
+		delete(recordmap, key)
+	}
+
 }
